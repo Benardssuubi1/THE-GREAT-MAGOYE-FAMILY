fd9c3a8b125a5d162b0e18867099323e9f9d645a
Benardssuubi1 <bensuubi614@gmail.com>
2026-06-08 15:11:45 +0300

    Add files via upload

---
+++ assets/extract-b64.ps1
@@ -0,0 +1,4 @@
+$line = Get-Content .\assets\hero-bg-line.txt
+$b64 = $line -replace ".*url\('data:image/jpeg;base64,", "" -replace "'\).*", ""
+[IO.File]::WriteAllText('.\assets\hero-bg-b64.txt', $b64)
+Write-Output 'saved'
